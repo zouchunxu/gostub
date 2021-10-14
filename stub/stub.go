57706c83c33605f6e69f0bd9b150b2b407b88e53
Chunxu Zou <471500439@qq.com>
2021-10-14 23:19:20 +0800

    up

--- stub/stub.go
+++ stub/stub.go
@@ -2,6 +2,7 @@ package stub
 
 import (
 	"reflect"
+	"syscall"
 	"unsafe"
 )
 
@@ -10,8 +11,31 @@ func Patch(target, replacement interface{}) {
 	to := reflect.ValueOf(replacement).Pointer()
 
 	data := jmp(to)
-	f := rawMemoryAccess(from, len(data))
-	copy(f, data)
+	copyTo(from, data)
+	//f := rawMemoryAccess(from, len(data))
+	//copy(f, data)
+}
+
+func copyTo(p uintptr, data []byte) {
+	f := rawMemoryAccess(p, len(data))
+
+	mprotectCrossPage(p, len(data), syscall.PROT_READ|syscall.PROT_EXEC|syscall.PROT_WRITE)
+	copy(f, data[:])
+	mprotectCrossPage(p, len(data), syscall.PROT_READ|syscall.PROT_EXEC)
+}
+
+func mprotectCrossPage(addr uintptr, length int, prot int) {
+	pageSize := syscall.Getpagesize()
+	for p := pageStart(addr); p < addr+uintptr(length); p += uintptr(pageSize) {
+		page := rawMemoryAccess(p, pageSize)
+		if err := syscall.Mprotect(page, prot); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func pageStart(ptr uintptr) uintptr {
+	return ptr & ^(uintptr(syscall.Getpagesize() - 1))
 }
 
 func rawMemoryAccess(p uintptr, length int) []byte {
