f58f774db16e27da3ea8f23cc9c73b7228239dbe
Chunxu Zou <471500439@qq.com>
2021-10-17 00:16:04 +0800

    imp add func

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"test/asm"
-)
+import "fmt"
 
 func test(a []int) {
 	a[2] = 4
@@ -13,11 +10,19 @@ func sum(a, b int) int {
 	return a + b
 }
 
-//monr
+var helloworld = "你好, 世界"
+
+func Add(int64, int64) int64
+
 func main() {
-	fmt.Println(asm.Id)
-	fmt.Println(asm.Name)
-	//_ = g.Getg() // 需要修改 go-tls 源码，原 g.getg 没有导出
-	//stub.Patch(sum, func(a, b int) int { return a - b })
-	//fmt.Println(sum(1, 2)) // 输出 -1
+	fmt.Println(Add(1, 2))
 }
+
+////monr
+//func main() {
+//	fmt.Println(asm.Id)
+//	fmt.Println(asm.Name)
+//	//_ = g.Getg() // 需要修改 go-tls 源码，原 g.getg 没有导出
+//	//stub.Patch(sum, func(a, b int) int { return a - b })
+//	//fmt.Println(sum(1, 2)) // 输出 -1
+//}
