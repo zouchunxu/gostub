7f3d76d9e87ffd5feb3b99083286220c0a106a9e
Chunxu Zou <471500439@qq.com>
2021-10-17 20:00:37 +0800

    if imp

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func test(a []int) {
-	a[2] = 4
-}
+var num [2]int
 
-func sum(a, b int) int {
-	return a + b
-}
+var (
+	boolValue  bool
+	trueValue  bool
+	falseValue bool
+)
 
-var helloworld = "你好, 世界"
+var int32Value int32
+
+func Swap(a, b int) (int, int)
 
 func Add(int64, int64) int64
 
-func main() {
+func output(s int) {
+	println(s)
+}
+
+func output2(a, b int) {
+	println(a, b)
+}
+
+func main()
+
+func mainA() {
+
+	a := 1
+	b := 2
+	c, d := Swap(a, b)
+	fmt.Println(c, d)
+	fmt.Println("end")
+
+	fmt.Println(num[0])
+
+	fmt.Println(boolValue)
+	fmt.Println(trueValue)
+	fmt.Println(falseValue)
+	fmt.Println(int32Value)
+
 	fmt.Println(Add(1, 2))
 }
 
--- main_amd64.s
+++ main_amd64.s
@@ -1,4 +1,22 @@
 #include "go_asm.h"
+#include "textflag.h"
+
+GLOBL ·num(SB),8,$16
+DATA ·num+0(SB)/8,$1
+DATA ·num+8(SB)/8,$0
+
+GLOBL ·boolValue(SB),8,$1
+
+GLOBL ·trueValue(SB),8,$1
+DATA ·trueValue+0(SB)/1,$1
+
+GLOBL ·falseValue(SB),8,$1
+DATA ·falseValue(SB)/1,$0
+
+GLOBL ·int32Value(SB),8,$4
+DATA ·int32Value+0(SB)/1,$0x01  // 第0字节
+DATA ·int32Value+1(SB)/1,$0x02  // 第1字节
+DATA ·int32Value+2(SB)/2,$0x03  // 第3-4字节
 
 TEXT ·Add(SB), $0-24
     MOVQ arg1+0(FP), CX
@@ -6,3 +24,58 @@ TEXT ·Add(SB), $0-24
     ADDQ CX, BP
     MOVQ BP, result+16(FP)
     RET
+
+TEXT ·Swap(SB),$8-32
+    MOVQ a+0(FP), AX     // AX = a
+    MOVQ AX,t-8(SP)
+    ADDQ t-8(SP),AX
+    MOVQ b+8(FP), BX     // BX = b
+    MOVQ BX, ret0+16(FP) // ret0 = BX
+    MOVQ AX, ret1+24(FP) // ret1 = AX
+    RET
+
+TEXT ·main(SB),$24-0
+    MOVQ $0, a-8*2(SP) // a = 0
+    MOVQ $0, a-8*1(SP) // b = 0
+    MOVQ $10,AX
+    MOVQ AX,a-8*2(SP)
+
+    MOVQ AX,0(SP)
+    CALL ·output2(SB)
+
+    MOVQ a-8*2(SP), AX
+    MOVQ a-8*1(SP), BX
+
+    MOVQ AX,BX
+    ADDQ BX,BX
+    IMULQ AX,BX
+    MOVQ BX,b-8*1(SP)
+
+    MOVQ BX,0(SP)
+    MOVQ BX,8(SP)
+    CALL ·output2(SB)
+
+
+    MOVQ $0,0(SP)
+    MOVQ $2,8(SP)
+    MOVQ $3,16(SP)
+    CALL ·If(SB)
+
+    MOVQ 24(SP),AX
+    MOVQ AX,0(SP)
+    CALL ·output(SB)
+    RET
+
+TEXT ·If(SB),NOSPLIT,$0-32
+    MOVQ ok+8*0(FP), CX
+    MOVQ a+8*1(FP), AX
+    MOVQ b+8*2(FP), BX
+
+    CMPQ CX,$0
+    JZ L
+    MOVQ AX, ret+24(FP)
+    RET
+L:
+    MOVQ BX, ret+24(FP)
+    RET
+
